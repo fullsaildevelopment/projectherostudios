1e0024254a0c85e5a6b120324b9665af2c08b4a7
Arthur Parker <aeparker101@mail.com>
2018-07-25 11:19:29 -0400

    Last Moments of happiness

---
+++ Villain CES/Villain CES/Projectile_Component.h
@@ -0,0 +1,24 @@
+#pragma once
+#ifndef CES_PROJECTILE_COMPONENT_H
+#define CES_PROJECTILE_COMPONENT_H
+#include"stdafx.h"
+using namespace std;
+#include <list>
+
+enum eProjectileComponent
+{
+	COMPONENT_PROJECTILESMASK = 1 << 0,
+	COMPONENT_WOOD = 1 << 1,
+	COMPONENT_METAL = 1 << 2,
+	COMPONENT_CLIP = 1 << 3
+
+};
+struct Clips
+{
+	list<int> nBulletsAvailables;
+	vector<int> nBulletsFired;
+	vector<int> nDeadBullets;
+	vector<float> fAliveTime;
+	int nSizeofClipl
+};
+#endif
