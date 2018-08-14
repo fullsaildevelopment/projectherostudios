e3e4785845ab3b0bc410fc8fcbb01f3b9b4a1749
Zayn Bakir <ZFBakir@student.fullsail.edu>
2018-08-14 14:38:03 -0400

    Start on Audio System Intergration with Wwise

--- Villain CES/Villain CES/AudioSystem.h
+++ Villain CES/Villain CES/AudioSystem.h
@@ -1,12 +1,97 @@
 #pragma once
-#include"AK/Comm/AkCommunication.h"
-#include"AK/IBytes.h"
+
+#include"AK/SoundEngine/Common/AkMemoryMgr.h"
+#include"AK/SoundEngine/Common/AkModule.h"
+#include"AK/SoundEngine/Common/IAkStreamMgr.h"
+#include"AK/SoundEngine/Platforms/Windows/AkWinSoundEngine.h"
+#include"AK/SoundEngine/Common/AkStreamMgrModule.h"
+#include"AK/Tools/Common/AkPlatformFuncs.h"
+#include"AK/MusicEngine/Common/AkMusicEngine.h"
 #include"AK/SoundEngine/Common/AkSoundEngine.h"
-#include"AK/Tools/Common/AkVectors.h"
-#include"AK/Wwise/AudioPlugin.h"
-#include"AK/Wwise/Utilities.h"
-#include"AK/SoundEngine/Common/AkCommonDefs.h"
-#include"AK/SpatialAudio/Common/AkSpatialAudio.h"
-#include"AK/AkPlatforms.h"
-#include"AK/SoundFrame/SF.h"
-#include"AK/SoundFrame/SFObjects.h"
+#ifndef AK_OPTIMIZED
+// Only needed for debug mode
+#include <AK/Comm/AkCommunication.h>
+#endif //AK_OPTIMIZED
+using namespace AK;
+class GameSound
+{
+public:
+	GameSound();
+	~GameSound();
+	void IntiializeSystem();
+private:
+
+};
+
+GameSound::GameSound()
+{
+}
+
+GameSound::~GameSound()
+{
+}
+
+inline void GameSound::IntiializeSystem()
+{
+#pragma region
+	AkMemSettings myMemoryManager;
+	myMemoryManager.uMaxNumPools = 20;
+	int test = 0;
+	if (AK::MemoryMgr::Init(&myMemoryManager) != AK_Success)
+	{
+		// Memeory Manager failed to create
+		test = -1;
+	}
+#pragma endregion Memory Manager Init
+	
+#pragma region
+	AkStreamMgrSettings myStream;
+	AK::StreamMgr::GetDefaultSettings(myStream);
+	if (!AK::StreamMgr::Create(myStream))
+	{
+		test = -2;
+	}
+
+
+	AkDeviceSettings myDevice;
+	AK::StreamMgr::GetDefaultDeviceSettings(myDevice);
+	// AkFilePackageLowLevelIOBlocking.h find this file 
+
+#pragma endregion Stream Manager & Device Init
+
+#pragma region
+	AkInitSettings mySoundEngine;
+	AkPlatformInitSettings myPlatform;
+	AK::SoundEngine::GetDefaultInitSettings(mySoundEngine);
+	AK::SoundEngine::GetDefaultPlatformInitSettings(myPlatform);
+	if(AK::SoundEngine::Init(&mySoundEngine, &myPlatform) != AK_Success)
+	{
+		test = -3;
+	}
+#pragma endregion Sound System & Platform Init
+
+#pragma region
+	AkMusicSettings myMusicEngine;
+	AK::MusicEngine::GetDefaultInitSettings(myMusicEngine);
+
+	if (AK::MusicEngine::Init(&myMusicEngine) != AK_Success)
+	{
+		test = -4;
+	}
+
+#pragma endregion Music Engine Init
+
+#pragma region
+#ifndef AK_OPTIMIZED
+	AkCommSettings myCommunicator;
+	AK::Comm::GetDefaultInitSettings(myCommunicator);
+	if (AK::Comm::Init(myCommunicator) != AK_Success)
+	{
+		test = -5;
+	}
+#endif // !AK_OPTIMIZED
+
+#pragma endregion Communication Between Wwise & C++ Project
+}
+
+
