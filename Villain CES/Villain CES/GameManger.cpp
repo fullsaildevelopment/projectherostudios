45b7ccd33f231532222ef782c21f716e97e3dacc
Jonah Rogers <jsrogers@fullsail.edu>
2018-11-07 14:10:04 -0500

    Fix the animation spazzing out at the beginning of every run

--- Villain CES/Villain CES/GameManger.cpp
+++ Villain CES/Villain CES/GameManger.cpp
@@ -8260,7 +8260,12 @@ int CGameMangerSystem::RealLevelUpdate()
 						pcGraphicsSystem->InitAnimShaderData(pcGraphicsSystem->m_pd3dDeviceContext, tAnimVertexBuffer, tThisWorld.atMesh[nCurrentEntity], debugCamera->d3d_Position);
 					}
 
-					tThisWorld.atAnimation[nCurrentEntity].tTimer.GetLocalTime(tThisWorld.atAnimation[nCurrentEntity].tTimer.tSceneTimer, tThisWorld.atAnimation[nCurrentEntity].tTimer.localTime);
+					if (bNoMoving)
+					{
+						tThisWorld.atAnimation[nCurrentEntity].tTimer.GetLocalTime(tThisWorld.atAnimation[nCurrentEntity].tTimer.tSceneTimer, tThisWorld.atAnimation[nCurrentEntity].tTimer.localTime);
+					
+						tThisWorld.atAnimation[nCurrentEntity].tTimer.localTime = 0;
+					}
 				}
 #if CAELIS
 				else if (tThisWorld.atInputMask[nCurrentEntity].m_tnInputMask == (COMPONENT_CAELIS | COMPONENT_INPUTMASK)
